0029de9a58a8a9680044c1669548105894851e19
Lucas R Archangelo <lustreet@gmail.com>
2017-07-01 12:38:29 -0300

    fix url

--- src/app/cadastro/cadastro.service.ts
+++ src/app/cadastro/cadastro.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CadastroService {
 
-  url = "http://localhost:8080/database/incluir"
+  url = "https://kritiunstudy.herokuapp.com/database/incluir"
   constructor(private http: Http) { }
 
   incluirPessoa(pessoa: Pessoa){
